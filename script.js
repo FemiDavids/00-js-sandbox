112f903481a3acbfc58941edb15fc77c66718f27
David Onuoha <thefemidavids@gmail.com>
2024-02-12 21:12:46 +0100

    çommit

--- script.js
+++ script.js
@@ -7,4 +7,20 @@ console.log(courseTitle);
 let firstName = "Jonas";
 console.log(firstName);
 
-let lastName; //camelCase
+// naming conventions and rules for naming variable
+let lastName = "Maltida"; //can use camelCase (industry standard)
+let jonas_matilda = "JM"; //can use underscore
+let $function = 27; //can use dollar sign
+
+let person = "jonas"; //it's best not to use uppercase to start a variable
+let PI = 3.1415; //reserved keywords like constants cannot be used
+
+//make your variable name descriptive
+
+let job1 = "programmer"; //not descriptive
+let job2 = "teacher"; //not descriptive
+
+let myFirstJob = "Programmer"; //very descriptive
+let myCurrentJob = "Teacher"; //very descriptive
+
+console.log(myFirstJob);
