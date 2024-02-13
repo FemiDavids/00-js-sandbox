dbbdad5585780449a14f53c7ba2887264d771bbd
David Onuoha <thefemidavids@gmail.com>
2024-02-13 21:17:57 +0100

    code challenge #01

--- script.js
+++ script.js
@@ -104,3 +104,10 @@ const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2020;
 console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; //x = y = 10, x = 10. the assignment operator are executed from right-to-left
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
